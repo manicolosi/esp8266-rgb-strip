eac9ac529a426950d501d51f0e87648bba7db2c1
Mark A. Nicolosi <mark.a.nicolosi@gmail.com>
2017-01-26 21:43:40 -0600

    Property for setting rgb value

--- homie/src/main.cpp
+++ homie/src/main.cpp
@@ -1,11 +1,103 @@
+#include <Arduino.h>
 #include <Homie.h>
 
+static const uint8_t LED_PIN_R = D6;
+static const uint8_t LED_PIN_G = D7;
+static const uint8_t LED_PIN_B = D5;
+
+static const char * color_names[] = {"red", "green", "blue"};
+static const uint8_t color_pins[] = {D6, D7, D5};
+
+HomieNode stripNode("strip", "strip");
+
+void setup_outputs() {
+  for(int i = 0; i < 3; i++) {
+    pinMode(color_pins[i], OUTPUT);
+    digitalWrite(color_pins[i], LOW);
+  }
+}
+
+uint8_t color_to_pin(const String& color) {
+  uint8_t pin = -1;
+
+  for(int i = 0; i < 3; i++) {
+    if(strcmp(color.c_str(), color_names[i]) == 0) {
+      pin = color_pins[i];
+    }
+  }
+
+  return pin;
+}
+
+bool set_color(const String& color, const String& value) {
+  uint8_t pin = color_to_pin(color);
+
+  if(pin != -1) {
+    Homie.getLogger() << color << " set to " << value.toInt() << endl;
+    stripNode.setProperty(color).send(value);
+    analogWrite(pin, value.toInt());
+  } else {
+    Homie.getLogger() << color << " is invalid" << endl;
+    return false;
+  }
+
+  return true;
+}
+
+bool red_handler(const HomieRange& range, const String& value) {
+  return set_color("red", value);
+}
+
+bool green_handler(const HomieRange& range, const String& value) {
+  return set_color("green", value);
+}
+
+bool blue_handler(const HomieRange& range, const String& value) {
+  return set_color("blue", value);
+}
+
+bool rgb_handler(const HomieRange& range, const String& value) {
+  int delim1 = value.indexOf(",");
+  int delim2 = value.indexOf(",", delim1 + 1);
+
+  String r = value.substring(0, delim1);
+  String g = value.substring(delim1 + 1, delim2);
+  String b = value.substring(delim2 + 1);
+
+  set_color("red", r);
+  set_color("green", g);
+  set_color("blue", b);
+  stripNode.setProperty("rgb").send(value);
+
+  return true;
+}
+
 void setup() {
+  setup_outputs();
   Serial.begin(115200);
-  Homie.setFirmware("bare-minimum", "1.0.0"); // The "_" is not a typo! See Magic bytes
+  Homie_setFirmware("bare-minimum", "1.0.0");
   Homie.setup();
+
+  stripNode.advertise("red").settable(red_handler);
+  stripNode.advertise("green").settable(green_handler);
+  stripNode.advertise("blue").settable(blue_handler);
+  stripNode.advertise("rgb").settable(rgb_handler);
 }
 
 void loop() {
   Homie.loop();
 }
+
+// TOPICS:
+// strip0/red
+// strip0/green
+// strip0/blue
+// strip0/rgb
+//
+// strip0/hue
+// strip0/saturation
+// strip0/luminosity
+// strip0/hsl
+//
+// strip0/state (on/off)
+// strip0/mode (normal/party/whatever)
